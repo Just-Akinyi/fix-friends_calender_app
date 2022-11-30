c99938d48bd5eb49ca124af7c1c0ca7d5d7392d1
oluwasube <oluwasube@outlook.com>
2022-11-30 18:08:20 +0100

    create models and db

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restx import Api
 from .auth.authentication import auth_namespace
-from .users.user import event_namespace
+from .events.event import event_namespace
 from .config.config import config_dict
+from ..utils.database import db
+from ..models.events import Event
+from ..models.users import User
+from flask_migrate import Migrate
 
 
 
@@ -10,10 +14,19 @@ def create_app(config = config_dict['dev']):
     app = Flask(__name__)
     
     app.config.from_object(config)
+    db.init_app(app)
+    migrate = Migrate(app, db)
     api = Api(app)
     api.add_namespace(auth_namespace)
     api.add_namespace(event_namespace)
     
+    @app.shell_context_processor   
+    def make_shell_context():
+            return{
+                'db':db,
+                'User':User,
+                'Event':Event
+            }
     
     return app
     
